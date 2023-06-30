b5bf48c9a0c1045318078d5396ac152b8864c9bb
Vansh Jain <vanshjain18.5.2002@gmail.com>
2023-06-30 10:48:26 -0700

    Exam : components

--- Components/navbar/navbar.component.ts
+++ Components/navbar/navbar.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+   isLoggedIn=false;
+   user=null;
+   role='';
+
+  constructor(public login:LoginService,private router:Router) {}
+  ngOnInit(): void {
+    this.isLoggedIn=this.login.isLoggedIn();
+    this.user=this.login.getUser();
+    this.login.loginStatusSubject.asObservable().subscribe( 
+      (data) => {
+        this.isLoggedIn=this.login.isLoggedIn();
+        this.user=this.login.getUser();
+      }
+    )
+  }
+
+
+  public logout() {
+    this.login.logout();
+    window.location.reload();
+    // this.login.loginStatusSubject.next(false);
+  }
+
+  public navigate(){
+    this.role = this.login.getUserRole();
+    if(this.role=='ADMIN'){
+      this.router.navigate(['admin/profile']);
+    }
+    else if(this.role=='NORMAL USER'){
+      this.router.navigate(['user-dashboard/user-profile']);
+    }
+    else{
+      return;
+    }
+    
+  }
 
 }
